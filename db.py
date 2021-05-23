7c5d724d211670a1c7b670b9dacfcc2cefdbd854
beeali68 <63969505+beeali68@users.noreply.github.com>
2021-05-23 04:22:53 +0300

    ali

---
+++ db.py
@@ -0,0 +1,58 @@
+import mysql.connector
+import xlrd
+
+print("VERİTABANI YÜKLENİYOR..")
+mydb = mysql.connector.connect(
+  host="remotemysql.com",
+  user="P0xYObZY55",
+  password="3bT6PmoVEb",
+  database="P0xYObZY55"
+)
+
+mycursor = mydb.cursor()
+
+#mycursor.execute("CREATE DATABASE IF NOT EXISTS xxxxxxxxx")
+
+
+
+wb = xlrd.open_workbook('ProjeCelikKirisTasarim/a.xls')
+sheet = wb.sheet_by_index(0)
+sheet.cell_value(0, 0)
+baslik=sheet.row_values(0)
+print(baslik)
+
+""" # başlık eklemek için
+mycursor.execute("CREATE TABLE IF NOT EXISTS a (kesit VARCHAR(50) PRIMARY KEY)".format(sheet.row_values(0)[0]))
+for i in range(1,27):
+  mycursor.execute("ALTER TABLE a ADD COLUMN {} TEXT".format(sheet.row_values(0)[i]))
+  mydb.commit() """
+
+
+
+""" for j in range(1,111):
+  print(j)
+  satir=sheet.row_values(j)
+  print(satir)
+  mycursor.execute("INSERT INTO a VALUE ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(satir[0], satir[1], satir[2], satir[3], satir[4], satir[5], satir[6], satir[7], satir[8], satir[9], satir[10], satir[11], satir[12], satir[13], satir[14], satir[15], satir[16], satir[17], satir[18], satir[19], satir[20], satir[21], satir[22], satir[23], satir[24], satir[25], satir[26]))
+  mydb.commit() """
+
+
+
+""" for j in range(1,111):
+  print(j)
+  satir=sheet.row_values(j)
+  print(satir)
+  for i in range(27):
+    print(baslik[i], satir[i])
+    mycursor.execute("INSERT INTO a ('{0}') VALUE ('{1}',)".format(baslik[i], satir[i]))
+    mydb.commit() """
+
+
+
+
+
+
+mycursor.execute("SELECT * FROM a")
+for x in mycursor.fetchall():
+    print(x)
+mydb.commit()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import mysql.connector
+from flask import Flask, render_template, request
+import xlrd
+
 
 
 print("VERİTABANI YÜKLENİYOR..")
+
 mydb = mysql.connector.connect(
   host="remotemysql.com",
   user="P0xYObZY55",
@@ -11,6 +15,27 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-#mycursor.execute("CREATE DATABASE IF NOT EXISTS xxxxxxxxx")
 
-mycursor.execute("CREATE TABLE IF NOT EXISTS a (id INT AUTO_INCREMENT PRIMARY KEY, isim VARCHAR(25))")
+
+app = Flask(__name__)
+
+@app.route('/', methods = ["POST","GET"])
+def giris_ekran():
+    mycursor.execute("SELECT * from a")   
+    satir = mycursor.fetchall() 
+    print(satir)
+
+    wb = xlrd.open_workbook('ProjeCelikKirisTasarim/a.xls')
+    sheet = wb.sheet_by_index(0)
+    sheet.cell_value(0, 0)
+    baslik=sheet.row_values(0)
+    print(baslik)
+    return render_template("a_tablo.html", rows = satir, cols = baslik) 
+
+
+
+
+if __name__ == '__main__':
+    #app.debug = True
+    app.run(host="0.0.0.0", port="8080")
+  
