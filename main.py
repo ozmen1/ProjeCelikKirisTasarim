c1fae9bf421103126a24c233390c70883317c7d9
beeali68 <63969505+beeali68@users.noreply.github.com>
2021-05-31 23:14:43 +0300

    ali

--- main.py
+++ main.py
@@ -86,7 +86,6 @@ def yontem_1():
                 # kesit değiştirme yapılacak
                 uygun_mu="UYGUN DEĞİL"
                 print(uygun_mu)
-                pass
 
        
             return render_template("a_tablo.html", satir_liste=satir_liste, p=p, l=l, kesit=kesit, celik_sinifi=celik_sinifi, uygun_mu = uygun_mu) 
@@ -96,91 +95,99 @@ def yontem_1():
             return render_template("a_tablo.html", satir_liste=satir_liste, uygun_mu=uygun_mu) 
 
 
-
+################################################################################################--2
 
 
 @app.route("/yontem_2",methods = ["POST","GET"])  
 def yontem_2():
-    if request.method == "POST":  
-        try:  
-            p = request.form["p"] 
-            l = request.form["l"] 
-            celik_sinifi = request.form["celik_sinifi"]
-            kesit_tipi = request.form["kesit_tipi"]
+    if request.method == "POST":
+      try:
+        p = request.form["p"] 
+        l = request.form["l"] 
+        celik_sinifi = request.form["celik_sinifi"]
+        kesit_tipi = request.form["kesit_tipi"]
+    
+
+        p=float(p)
+        l=float(l)
+        celik_sinifi=float(celik_sinifi)
+
+        maksimum_moment=(p*l)/(4)
+        #print("maksimum moment",maksimum_moment)
+
+        wp_gerekli=maksimum_moment*1.67/celik_sinifi
+        #print("Wp gerekli : ",wp_gerekli)
+
+        mycursor.execute("SELECT * FROM a")
+        satir= mycursor.fetchall()
+        mydb.commit()
+
+        satir_dict={}
+
+        for i in range(len(satir)):
+            #print(satir[i][0])
+            #print(kesit_tipi)
+
+            if kesit_tipi in satir[i][0]:
+                satir_dict[satir[i][0]]=satir[i][17]
+        #print(satir_dict)
         
-
-            p=float(p)
-            l=float(l)
-            celik_sinifi=float(celik_sinifi)
-            global key_liste
-
-            maksimum_moment=(p*l)/(4)
-            #print("maksimum moment",maksimum_moment)
-
-            wp_gerekli=maksimum_moment*1.67/celik_sinifi
-            #print("Wp gerekli : ",wp_gerekli)
-
-            mycursor.execute("SELECT * FROM a")
-            satir= mycursor.fetchall()
-            mydb.commit()
-
-            satir_dict={}
-
-            for i in range(len(satir)):
-                #print(satir[i][0])
-                #print(kesit_tipi)
-
-                if kesit_tipi in satir[i][0]:
-                    satir_dict[satir[i][0]]=satir[i][17]
-            #print(satir_dict)
-
-            fark=10000000000
-            for x, y in satir_dict.items():                
-                #print(x, y)
-                if wp_gerekli<=float(y):
-                    fark_temp=float(y)-wp_gerekli
-                    if fark>fark_temp:
-                        fark=fark_temp
-                        kesit_2=x
-            #print(kesit_2)
+        while True:
+          fark=10000000000
+          for x, y in satir_dict.items():                
+              #print(x, y)
+              if wp_gerekli<=float(y):
+                  fark_temp=float(y)-wp_gerekli
+                  if fark>fark_temp:
+                      fark=fark_temp
+                      kesit_2=x
+          #print(kesit_2)
 
 
-            mycursor.execute("SELECT * FROM a WHERE kesit='{}'".format(kesit_2))
-            satir_2= mycursor.fetchall()
-            #print(satir)
-            mydb.commit()
+          mycursor.execute("SELECT * FROM a WHERE kesit='{}'".format(kesit_2))
+          satir_2= mycursor.fetchall()
+          #print(satir)
+          mydb.commit()
 
-            zati_sehim=((5*float(satir_2[0][1])*(9.81/1000)*(l*1000)**4)/(384*200000*(float(satir_2[0][15])*(10**4))))
-            #print(zati_sehim)
+          zati_sehim=((5*float(satir_2[0][1])*(9.81/1000)*(l*1000)**4)/(384*200000*(float(satir_2[0][15])*(10**4))))
+          #print(zati_sehim)
 
-            zati_moment=(float(satir_2[0][1])*9.81*(l**2))/(8)
-            #print(zati_moment)
+          zati_moment=(float(satir_2[0][1])*9.81*(l**2))/(8)
+          #print(zati_moment)
 
-            maksimum_sehim=(p*(l*1000)**3)/(48*200000*float(satir_2[0][15])*(10**4))
-            #print(maksimum_sehim)
+          maksimum_sehim=(p*(l*1000)**3)/(48*200000*float(satir_2[0][15])*(10**4))
+          #print(maksimum_sehim)
 
-            toplam_sehim=( zati_sehim + maksimum_sehim)
-            #print(toplam_sehim)
+          toplam_sehim=( zati_sehim + maksimum_sehim)
+          #print(toplam_sehim)
 
-            toplam_moment=(maksimum_moment+zati_moment)
-            #print(toplam_moment)
+          toplam_moment=(maksimum_moment+zati_moment)
+          #print(toplam_moment)
 
-            akma_sinir_durumu=(celik_sinifi*float(satir_2[0][17])/1.67)
-            #print(akma_sinir_durumu)
+          akma_sinir_durumu=(celik_sinifi*float(satir_2[0][17])/1.67)
+          #print(akma_sinir_durumu)
 
-            if ((akma_sinir_durumu > toplam_moment) and (l*1000/300) > toplam_sehim):
-                uygun_mu="UYGUN"
-                print(uygun_mu)
-            else:
-                # kesit değiştirme yapılacak
-                uygun_mu="UYGUN DEĞİL"
-                print(uygun_mu)
-                     
-            return render_template("yontem_2.html", kesitt=kesit_2, uygun_mu=uygun_mu) 
-        except:
-            uygun_mu="GEÇERLİ DEĞER GİRİLMEDİ"
-            print(uygun_mu)
-            return render_template("yontem_2.html") 
+          if ((akma_sinir_durumu > toplam_moment) and (l*1000/300) > toplam_sehim):
+              uygun_mu="UYGUN"
+              print(uygun_mu)
+              return render_template("yontem_2.html", kesitt=kesit_2, uygun_mu=uygun_mu)
+          else:
+              # kesit değiştirme yapılacak
+              # uygun_mu="UYGUN DEĞİL"
+              # print(uygun_mu)
+              satir_dict.pop(kesit_2)
+              continue                 
+          
+      except:
+        uygun_mu="GEÇERLİ DEĞER GİRİLMEDİ"
+        print(uygun_mu)
+        return render_template("yontem_2.html", uygun_mu=uygun_mu)
+
+
+
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from flask import Flask, render_template, request
 
+
 print("VERİTABANI YÜKLENİYOR..")
 
 mydb = mysql.connector.connect(
@@ -9,110 +10,218 @@ mydb = mysql.connector.connect(
   password="3bT6PmoVEb",
   database="P0xYObZY55"
 )
+
 mycursor = mydb.cursor()
 
 
 
-def max_sehim():
-  p=float(input("P değerini giriniz: "))
-  l=float(input("L değerini giriniz: "))
-  kesit=(input("kesit giriniz"))
-  mycursor.execute("SELECT * FROM a WHERE kesit='{}'".format(kesit))
-  satir= mycursor.fetchall()
-  # print(satir)
-  # print(satir[0][15])
-  mydb.commit()
+app = Flask(__name__)
 
-  maksimum_sehim=(p*(l**3))/(48*200000*float(satir[0][15])*(10**4))
-  print("MAKSİMUM SEHİM : ", float(maksimum_sehim))
-  return maksimum_sehim
+@app.route('/')
+def giris_ekran():
 
+    global satir_liste
 
+    mycursor.execute("SELECT kesit FROM a")
+    satir_temp= mycursor.fetchall()
+    satir_liste=[]
+    for i in range(len(satir_temp)):
+        #print(satir_temp[i][0])
+        satir_liste.append(satir_temp[i][0])
+    #print(satir_liste)
+    mydb.commit()
 
+    return render_template("a_tablo.html", satir_liste=satir_liste) 
 
-def max_moment():
-  p=float(input("P değerini giriniz: "))
-  l=float(input("L değerini giriniz: "))
-
-  maksimum_moment=(p*l*1000)/(4)
-
-  print("MAKSİMUM MOMENT : ", float(maksimum_moment))
-  return maksimum_moment
+@app.route('/yontem_2.html')
+def yontem2():
 
 
-
-def zati_sehim():
-  l=float(input("L değerini giriniz: "))
-  kesit=(input("kesit giriniz"))
-  mycursor.execute("SELECT * FROM a WHERE kesit='{}'".format(kesit))
-  satir= mycursor.fetchall()
-  # print(satir)
-  # print(satir[0][1])
-  # print(satir[0][15])
-  mydb.commit()
-
-  zati_sehim=((5*float(satir[0][1])*(9.81/1000)*(l**4))/(384*200000*(float(satir[0][15])*(10**4))))
-  print("zati sehim : ", zati_sehim)
-  return zati_sehim
+    return render_template("yontem_2.html") 
 
 
+@app.route("/yontem_1",methods = ["POST","GET"])  
+def yontem_1():
+    if request.method == "POST":  
+        try:  
+            p = request.form["p"] 
+            l = request.form["l"] 
+            celik_sinifi = request.form["celik_sinifi"]
+            kesit = request.form["kesit"]
+
+            p=float(p)
+            l=float(l)
+            celik_sinifi=float(celik_sinifi)
+
+            mycursor.execute("SELECT * FROM a WHERE kesit='{}'".format(kesit))
+            satir= mycursor.fetchall()
+            print(satir)
+            mydb.commit()
+
+            maksimum_sehim=(p*(l*1000)**3)/(48*200000*float(satir[0][15])*(10**4))
+            print(maksimum_sehim)
+
+            maksimum_moment=(p*l)/(4)
+            print(maksimum_moment)
+
+            zati_sehim=((5*float(satir[0][1])*(9.81/1000)*(l*1000)**4)/(384*200000*(float(satir[0][15])*(10**4))))
+            print(zati_sehim)
+
+            zati_moment=(float(satir[0][1])*9.81*(l**2))/(8)
+            print(zati_moment)
+
+            toplam_sehim=( zati_sehim + maksimum_sehim)
+            print(toplam_sehim)
+
+            toplam_moment=(maksimum_moment+zati_moment)
+            print(toplam_moment)
+
+            akma_sinir_durumu=(celik_sinifi*float(satir[0][17])/1.67)
+            print(akma_sinir_durumu)
+
+            if ((akma_sinir_durumu > toplam_moment) and (l*1000/300) > toplam_sehim):
+                uygun_mu="UYGUN"
+                print(uygun_mu)
+            else:
+                # kesit değiştirme yapılacak
+                uygun_mu="UYGUN DEĞİL"
+                print(uygun_mu)
+
+       
+            return render_template("a_tablo.html", satir_liste=satir_liste, p=p, l=l, kesit=kesit, celik_sinifi=celik_sinifi, uygun_mu = uygun_mu) 
+        except:
+            uygun_mu="GEÇERLİ DEĞER GİRİLMEDİ"
+            print(uygun_mu)
+            return render_template("a_tablo.html", satir_liste=satir_liste, uygun_mu=uygun_mu) 
 
 
-def zati_moment():
-  l=float(input("L değerini giriniz: "))
-  kesit=(input("kesit giriniz"))
-  mycursor.execute("SELECT * FROM a WHERE kesit='{}'".format(kesit))
-  satir= mycursor.fetchall()
-  # print(satir)
-  # print(satir[0][1])
-  mydb.commit()
-  zati__moment=(float(satir[0][1])*(l**2))/(8)
-  print("ZATİ MOMENT : ",zati__moment)
-  return zati__moment
+################################################################################################--2
 
 
-def toplam_sehim():
-  # def zati_sehim():  ile  def max_sehim():   toplanacak 
+@app.route("/yontem_2",methods = ["POST","GET"])  
+def yontem_2():
+    if request.method == "POST":
+      try:
+        p = request.form["p"] 
+        l = request.form["l"] 
+        celik_sinifi = request.form["celik_sinifi"]
+        kesit_tipi = request.form["kesit_tipi"]
+    
 
-  toplam_sehim=( zati_sehim() + max_sehim())
-  print("TOPLAM SEHİM : ", toplam_sehim)
-  return toplam_sehim
+        p=float(p)
+        l=float(l)
+        celik_sinifi=float(celik_sinifi)
+
+        maksimum_moment=(p*l)/(4)
+        #print("maksimum moment",maksimum_moment)
+
+        wp_gerekli=maksimum_moment*1.67/celik_sinifi
+        #print("Wp gerekli : ",wp_gerekli)
+
+        mycursor.execute("SELECT * FROM a")
+        satir= mycursor.fetchall()
+        mydb.commit()
+
+        satir_dict={}
+
+        for i in range(len(satir)):
+            #print(satir[i][0])
+            #print(kesit_tipi)
+
+            if kesit_tipi in satir[i][0]:
+                satir_dict[satir[i][0]]=satir[i][17]
+        #print(satir_dict)
+        
+        while True:
+          fark=10000000000
+          for x, y in satir_dict.items():                
+              #print(x, y)
+              if wp_gerekli<=float(y):
+                  fark_temp=float(y)-wp_gerekli
+                  if fark>fark_temp:
+                      fark=fark_temp
+                      kesit_2=x
+          #print(kesit_2)
 
 
-def toplam_moment():
-  # def max_moment(): ile   def zati_moment():   toplanacak
+          mycursor.execute("SELECT * FROM a WHERE kesit='{}'".format(kesit_2))
+          satir_2= mycursor.fetchall()
+          #print(satir)
+          mydb.commit()
 
-  toplam_moment=(max_moment()+zati_moment())
-  print("TOPLAM MOMENT : ", toplam_moment)
+          zati_sehim=((5*float(satir_2[0][1])*(9.81/1000)*(l*1000)**4)/(384*200000*(float(satir_2[0][15])*(10**4))))
+          #print(zati_sehim)
+
+          zati_moment=(float(satir_2[0][1])*9.81*(l**2))/(8)
+          #print(zati_moment)
+
+          maksimum_sehim=(p*(l*1000)**3)/(48*200000*float(satir_2[0][15])*(10**4))
+          #print(maksimum_sehim)
+
+          toplam_sehim=( zati_sehim + maksimum_sehim)
+          #print(toplam_sehim)
+
+          toplam_moment=(maksimum_moment+zati_moment)
+          #print(toplam_moment)
+
+          akma_sinir_durumu=(celik_sinifi*float(satir_2[0][17])/1.67)
+          #print(akma_sinir_durumu)
+
+          if ((akma_sinir_durumu > toplam_moment) and (l*1000/300) > toplam_sehim):
+              uygun_mu="UYGUN"
+              print(uygun_mu)
+              return render_template("yontem_2.html", kesitt=kesit_2, uygun_mu=uygun_mu)
+          else:
+              # kesit değiştirme yapılacak
+              # uygun_mu="UYGUN DEĞİL"
+              # print(uygun_mu)
+              satir_dict.pop(kesit_2)
+              continue                 
+          
+      except:
+        uygun_mu="GEÇERLİ DEĞER GİRİLMEDİ"
+        print(uygun_mu)
+        return render_template("yontem_2.html", uygun_mu=uygun_mu)
 
 
 
-def akma_sinir_durumu():
-  kesit=(input("kesit giriniz"))
-  celik_sinifi=float(input("ÇELİK SINIFI DEĞERİNİ GİRİNİZ"))
-  mycursor.execute("SELECT * FROM a WHERE kesit='{}'".format(kesit))
-  satir= mycursor.fetchall()
-  # print(satir[0][17])
-  mydb.commit()
-  a_s_durumu=(celik_sinifi*float(satir[0][17])/1.67)
-  print("AKMA SINIR DURUMU : ", a_s_durumu)
-  return a_s_durumu
-
-
-if (akma_sinir_durumu > toplam_moment) and (l/300 > toplam_sehim):
-  print("uygun")
-else:
-  # kesit değiştirme yapılacak
-  pass
 
 
 
 
 
-# max_sehim()
-# max_moment()
-# zati_sehim()
-# zati_moment()
-# toplam_sehim()
-# toplam_moment()
-# akma_sinir_durumu()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    #app.debug = True
+    app.run(host="0.0.0.0", port="8080")
+  
+
+
+
+
+
