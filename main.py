a52ba475c5cb59df3a8d397ff2b63a677bb4eebe
beeali68 <63969505+beeali68@users.noreply.github.com>
2021-07-11 23:16:34 +0300

    ali

--- main.py
+++ main.py
@@ -519,5 +519,5 @@ def yontem_2():
 
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run(host="0.0.0.0", port="5000")
--- static/myscript.js
+++ static/myscript.js
@@ -64,3 +64,14 @@ function gizle() {
 
 }
 
+function printDiv() {
+    var divContents = document.getElementById("aciklama").innerHTML;
+    var a = window.open('', '', 'height=500, width=500');
+    a.document.write('<html>');
+    a.document.write('<body ><br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
+
